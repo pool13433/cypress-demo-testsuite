77d36e906a7f6534cddacaa2f13be02f1173e852
Poolsawat Apin <poon_mp@hotmail.com>
2020-06-27 13:41:13 +0700

    Update exercise2.spec.js
    add loop

--- cypress/integration/workshop/day1/exercise2.spec.js
+++ cypress/integration/workshop/day1/exercise2.spec.js
@@ -4,12 +4,35 @@
 3. ใช้ commands (as ,check ,click ,contains ,get ,log ,not) มาประยุกต์เพิ่มเติมการทำงานของเคสข้างล่างนี้
     (เพิ่มเติมหากมีเวลาเหลือ)
 */
+const persons = require('./persons.json')
 
 describe('ทดสอบการใช้งานร่วมกันของ command ต่าง ๆ', () => {
-  it('เขียน testscript ใช้คำสั่ง .get() .type() เพิ่มค่าลงใน form ให้ครบถ้วน', () => {
-    cy.visit('/register.html')   
-    .get('#firstName').type('poolsawat')
-    // เพิ่มโค๊ดของคุณที่นี่
-    //.get()
+
+  persons.forEach((person, index) => {
+    it('เขียน testscript ใช้คำสั่ง .get() .type() ของคุณ '+person.firstName, () => {
+      cy.visit('/register.html')
+        .get('#firstName').type(person.firstName)
+        // เพิ่มโค๊ดของคุณที่นี่
+        .get('#lastName').type(person.lastName)
+        .get('#phone').type('0800000000')
+        .get('#email').type(person.email)
+        .get('#age').type(person.age)
+        .get('#address').type('กทม')
+
+        .then(()=>{
+          if(person.gender == 'M'){
+            cy.get('#male').check()
+          }else{
+            cy.get('#female').check()
+          }          
+        })
+        .get('#occupation').select('Doctor')
+
+        .get('#birthDate').type('2020-04-10')
+
+
+        .get('#register').click()
+    })
   })
 })
+
